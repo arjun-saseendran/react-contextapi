90f010a43a4a53d24f11da615aaf60ade1517d67
arjun-saseendran <arjunsaseendran@icloud.com>
2024-08-17 04:35:11 +0530

    add Login

---
+++ src/components/Login.jsx
@@ -0,0 +1,43 @@
+import React, {useState} from 'react'
+
+const Login = () => {
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+
+	const handleSubmit = () => {
+
+	}
+
+return(
+
+
+<div>
+	<h2>Login</h2>
+	<input
+type='text'
+value={username}
+onChange={(e) => setUsername(e.target.value)}
+placeholder='Username'
+
+	/>
+	{' '}
+	<input
+type='password'
+value={password}
+onChange={(e) => setPassword(e.target.value)}
+placeholder='Password'
+	/>
+<button>
+onClick={handleSubmit}
+
+Submit
+</button>
+
+</div>
+
+
+	)
+
+}
+
+export default Login;
